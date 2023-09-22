1be58fc41ed60599356230abeea0152252495d65
sksiyer <sarahksiyer@gmail.com>
2023-09-22 22:57:46 +0100

    cleanup

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,20 +1,18 @@
 import logo from "../../resources/img/logo_light.png";
 import {Form, Link} from "react-router-dom";
-import React, {useContext} from "react";
-import {UserContext} from "../../App";
+import React from "react";
+
 
 export default function Login(){
-  const {name} = useContext(UserContext)
   return <>
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
-          className="mx-auto h-10 w-auto"
           src={logo}
           alt="Company Logo"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account {name}
+          Sign in to your account
         </h2>
       </div>
 
--- src/components/Timesheet/CalendarInputTable.tsx
+++ src/components/Timesheet/CalendarInputTable.tsx
@@ -2,34 +2,6 @@ import {Card, Typography} from "@material-tailwind/react";
 
 const TABLE_HEAD = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-const TABLE_ROWS = [
-  {
-    name: "John Michael",
-    job: "Manager",
-    date: "23/04/18",
-  },
-  {
-    name: "Alexa Liras",
-    job: "Developer",
-    date: "23/04/18",
-  },
-  {
-    name: "Laurent Perrier",
-    job: "Executive",
-    date: "19/09/17",
-  },
-  {
-    name: "Michael Levi",
-    job: "Developer",
-    date: "24/12/08",
-  },
-  {
-    name: "Richard Gran",
-    job: "Manager",
-    date: "04/10/21",
-  },
-];
-
 export default function CalendarInputTable() {
   return (
     <Card className="rounded-lg border overflow-scroll items-center border-blue-gray-100 bg-gray-50">
